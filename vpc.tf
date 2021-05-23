307aac79681fdb41b4d28f49b89ff1809b376575
pankaj89-github <pankaj.hasija@gmail.com>
2021-05-23 16:30:52 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/pankaj89-training/network/google"
+  version = "2.5.0"
+  network_name = pankaj-network
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
