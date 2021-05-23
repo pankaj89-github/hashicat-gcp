908d9b3fd3bb7b6342cfaa162d2c0156c1ef6aa1
pankaj89-github <84673814+pankaj89-github@users.noreply.github.com>
2021-05-23 22:08:03 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 module "network" {
   source  = "app.terraform.io/pankaj89-training/network/google"
   version = "2.5.0"
-  network_name = pankaj-network
+  network_name = "pankaj-network"
   project_id = var.project
   subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "pankaj-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
